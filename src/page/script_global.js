a5f9d29d513b622eb963882c632af8780759801c
RaSan147 <34002411+RaSan147@users.noreply.github.com>
2023-05-14 21:48:14 +0600

    fix  clear  cookies

--- src/page/script_global.js
+++ src/page/script_global.js
@@ -245,20 +245,11 @@ class Tools {
 	}
 	
 	clear_cookie() {
-    var cookies = document.cookie.split("; ");
-    for (var c = 0; c < cookies.length; c++) {
-        var d = window.location.hostname.split(".");
-        while (d.length > 0) {
-            var cookieBase = encodeURIComponent(cookies[c].split(";")[0].split("=")[0]) + '=; expires=Thu, 01-Jan-1970 00:00:01 GMT; domain=' + d.join('.') + ' ;path=';
-            var p = location.pathname.split('/');
-            document.cookie = cookieBase + '/';
-            while (p.length > 0) {
-                document.cookie = cookieBase + p.join('/');
-                p.pop();
-            }
-            d.shift();
-        }
-    }
+		document.cookie.split(";").forEach(
+			function(c) { 
+				document.cookie = c.replace(/^ +/, "").replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/"); 
+			}
+		);
 	}
 }
 var tools = new Tools();
