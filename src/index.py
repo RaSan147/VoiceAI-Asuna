329924d06659cc4855f4c8bae009ab0c8ea77ca1
Ratul Hasan <34002411+RaSan147@users.noreply.github.com>
2024-01-22 22:37:20 +0600

    testing CWD

--- src/index.py
+++ src/index.py
@@ -1,12 +1,15 @@
 # print current path and file list
 
-import os
+import os, sys
 
 if not os.getcwd().endswith("src"):
 	os.chdir("./src")
+	sys.path[0] = os.path.dirname(os.path.abspath(__file__))
 
-
-from App_server import SH
+try:
+	from App_server import SH
+except:
+	raise Exception(os.getcwd(), os.listdir())
 
 
 handler = SH
