71f03d60574adcfa5265669af6c67731cc711dc5
Ratul Hasan <34002411+RaSan147@users.noreply.github.com>
2024-01-22 23:16:44 +0600

    updating error log

--- src/index.py
+++ src/index.py
@@ -1,15 +1,15 @@
 # print current path and file list
 
-import os, sys
+import os, sys, traceback
 
 if not os.getcwd().endswith("src"):
 	os.chdir("./src")
-	sys.path[0] = os.path.dirname(os.path.abspath(__file__))
+	sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
 try:
 	from App_server import SH
 except:
-	raise Exception(os.getcwd(), os.listdir())
+	raise Exception(os.getcwd(), os.listdir(), traceback.format_exc())
 
 
 handler = SH
